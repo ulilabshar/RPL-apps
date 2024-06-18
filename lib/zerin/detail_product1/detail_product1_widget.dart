618cb87fa2dee2812aaf2d34a2431d233544d69e
zerinaphilia23@students.unnes.ac.id <zerinaphilia23@students.unnes.ac.id>
2024-06-18 22:26:21 +0700

    desain visual model

--- lib/zerin/detail_product1/detail_product1_widget.dart
+++ lib/zerin/detail_product1/detail_product1_widget.dart
@@ -25,6 +25,7 @@ class DetailProduct1Widget extends StatefulWidget {
 class _DetailProduct1WidgetState extends State<DetailProduct1Widget> {
   late DetailProduct1Model _model;
 
+  //scaffoldKey for struktur dasar dari desain visual Material Design
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
